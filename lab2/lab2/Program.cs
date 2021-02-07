e93b1c87d9e40667d9ecc96a59639bcfbc223ea3
ZannatulFerdous1123 <zannatar1123@gmail.com>
2021-02-07 13:09:07 +0600

    lab tasks

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for(int i=0;i<=100; i++)
+            { sum = sum + i; }
+            Console.WriteLine("sum:" + sum);
+        }
+    }
+}
