2899878b082ec34cd409cde8f15897db4e8772f1
ZannatulFerdous1123 <zannatar1123@gmail.com>
2021-02-14 14:04:53 +0600

    labtask

---
+++ Array/Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main array ");
+            int[] arr = new int[] { 2, 4 };
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i] + " ");
+            Console.WriteLine( "copy array ");
+            int[] arr2 = new int[arr.Length];
+
+            for (int i=0;i<arr.Length; i++)
+            {
+                arr2[i] = arr[i];
+                Console.WriteLine(arr2[i] + " ");
+            }
+               
+         
+            
+
+
+
+
+        }
+    }
+}
---
+++ ArrayDuplicate/ArrayDuplicate/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 7,7,4,3 };
+            int count = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[0] == arr[i])
+                {
+                    count++;
+                }
+
+
+            }
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[1] == arr[i])
+                {
+                    count++;
+                }
+
+
+            }
+            for (int i = 3; i < arr.Length; i++)
+            {
+                if (arr[2] == arr[i])
+                {
+                    count++;
+                }
+
+
+            }
+            for (int i = 4; i < arr.Length; i++)
+            {
+                if (arr[3] == arr[i])
+                {
+                    count++;
+                }
+
+
+            }
+            for (int i = 5; i < arr.Length; i++)
+            {
+                if (arr[4] == arr[i])
+                {
+                    count++;
+                }
+
+
+            }
+
+            Console.WriteLine("Total number of duplicate number : " + count);
+        }
+    }
+}
